fb571024feb18d73e22685c872472245bfc99d2a
Thanh Le <tlle3295@gmail.com>
2025-03-24 18:57:19 -0400

    Fix: SQLite implementation for Android platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:io' show Platform;
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/register_screen.dart';
 import 'theme/app_theme.dart';
 
-void main() {
-  // Initialize FFI for Windows
-  sqfliteFfiInit();
-  // Set the databaseFactory to use FFI
-  databaseFactory = databaseFactoryFfi;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize SQLite
+  if (!kIsWeb) {
+    if (!Platform.isAndroid) {
+      // Use FFI for non-Android platforms
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi;
+    }
+    // Android will use the default sqflite implementation
+  }
 
   runApp(const MyApp());
 }
